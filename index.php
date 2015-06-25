1046ce9e40f8f00a90f6cead30d333fa3a03f2ae
Akrom <akirom123@ya.ru>
2015-06-25 14:56:29 +0300

    html layour

--- index.php
+++ index.php
@@ -3,10 +3,41 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<style>
+	body {
+		font-family: Arial, sans-serif;
+		padding: 20px;
+		margin: 0;
+	}
+		.news-item {
+			border: 1px solid #ccc;
+			margin: 20px 20px 20px 0;
+			float: left;
+			height: 250px;
+			width: 250px;
+			padding: 10px;
+		}
+		a {
+			color: red;
+			display: inline-block;
+			margin-bottom: 10px;
+		}
+		a:hover {
+			color: blue;
+		}
+		img {
+			max-width: 100%;
+			border-radius: 5px;
+		}
+	</style>
 </head>
 <body>
 	<h3>News List</h3>
-	<div></div>
-	<script></script>
+	<div class="news-item">
+		<a href="">Заголовок новости</a>
+		<img src="img/thumb.jpg" alt="">
+		<p>Текст</p>
+	</div>
+	<script src="js/main.js"></script>
 </body>
 </html>
